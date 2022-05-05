a5ff055c6c6175ab7338438a62edd3750ace39c1
Avoaja Joshua Akachi <akachijc@gmail.com>
2022-05-05 17:25:53 +0000

    solvesd build problem. --next=reduce build size

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./index.scss";
+import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RecoilRoot } from "recoil";
