2ecb300dc9cc93a9ec384b2d725410c39c8928c9
Logan <tushlogan@gmail.com>
2022-08-03 15:58:13 +0000

    font-weight-fix

--- src/components/AddFundsModal.jsx
+++ src/components/AddFundsModal.jsx
@@ -28,24 +28,24 @@ export default function AddFundsModal(props) {
                   </button>
                 </div>
                 <div className="modal-header mt-6">
-                  <h4 className="text text-center font-bold">Add funds</h4>
+                  <h4 className="text text-center font-[500]">Add funds</h4>
                 </div>
 
                 <div className="flex mt-4 justify-between">
                   <div className="border-b-[3px] cursor-pointer flex-1 font-bold border-b-brand-red">
-                    <p className="text modal-padding font-bold py-2">
+                    <p className="text modal-padding font-[500] py-2">
                       Deposit Crypto
                     </p>
                   </div>
                   <div className="cursor-not-allowed flex-1 flex justify-end">
-                    <p className="text text-[#858D9F] modal-padding py-2  font-bold ">
+                    <p className="text text-[#858D9F] modal-padding py-2  font-[500] ">
                       Fund with Card
                     </p>
                   </div>
                 </div>
 
                 <div className="modal-padding">
-                  <p className="text w-full max-w-[295px] text-center mt-8 font-[300px] mx-auto">
+                  <p className="text w-full max-w-[295px] text-center mt-8 font-[300] mx-auto">
                     Transfer funds from an exchange or another wallet to the
                     address below
                   </p>
